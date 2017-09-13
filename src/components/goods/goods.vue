8febfc5df18b69e0a1ded507a740ff8caf91bbbe
shihao <1010465466@qq.com>
2017-09-13 21:42:14 +0800

    添加商品的JS功能。

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -3,7 +3,7 @@
     <div class="goods">
       <div class="" ref="">
         <ul>
-          <li class="" ref="" v-for="(item,index) in goods" :class="" @click="">
+          <li class="" ref="" v-for="(item,index) in goods" :class="{'current':currentIndex=== index}" @click="">
              <span class="text">
                 <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
                 {{item.name}}
@@ -66,12 +66,62 @@
       return {
         goods: [],
         listHeight: [],
-        scrollY: 0,
-        selectedFood:{}
+        //滚动栏数
+        scroll: 0,
+        selectedFood: {}
       }
     },
     computed: {
       //TODO 未添加商品的计算
+      currentIndex() {
+        for (let i = 0; i < this.listHeight; i++) {
+          let height1 = this.listHeight[i];
+          let height2 = this.listHeight[i + 1];
+          //如果height2或者 滚动栏数在1和 2 之间
+          if (!height2 || (this.scroll >= height1 && this.scroll < height2)) {
+            return i;
+          }
+        }
+        return 0;
+      },
+      selectFoods() {
+        let foods = [];
+        this.goods.forEach(good => {
+          good.foods.forEach(food => {
+            if (food.count) {
+              foods.push(food);
+            }
+          });
+        });
+        return foods;
+      }
+    },
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+      this.$http.get('/api/goods').then((response) => {
+        response = response.body
+        if (response.errno === ERR_OK) {
+          this.goods = response.data;
+          this.$nextTick(() => {
+            this._initScroll();
+            this._calculateHeight();
+          });
+        }
+      });
+    },
+    methods:{
+      _initScroll(){
+        this.menuScroll = new BScroll(this.$refs.menuWrapper,{
+          click:true
+        });
+        this.foodScroll = new BScroll(this.$refs.foodWrapper,{
+          probeType:3,
+          click:true
+        });
+      },
+      _calculateHeight(){
+      this.$refs.foodWrapper.getElementsByClassName('food-list-hook');
+      }
     },
     components: {
       shop_cart,
